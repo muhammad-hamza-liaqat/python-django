26d93a881237f482d6407a99822db7637ab4c30b
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-07-21 18:55:01 +0500

    add: swagger integrated

--- authapp/views.py
+++ authapp/views.py
@@ -5,11 +5,14 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from server.utils import success_response, error_response
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg.utils import swagger_auto_schema
+
 
 
 class SignupView(APIView):
     permission_classes = [AllowAny]
-
+    
+    @swagger_auto_schema(request_body=RegisterSerializer)
     def post(self, request):
         serializer = RegisterSerializer(data=request.data)
         if serializer.is_valid():
--- server/settings.py
+++ server/settings.py
@@ -22,7 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'authapp', 
+    'authapp',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
