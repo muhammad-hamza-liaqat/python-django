23553a34ae62f5fc5d06596902e93a2416ef1914
Muhammad-Hamza-Liaqat <mh408800@gmail.com>
2025-08-04 20:30:02 +0500

    Refactor: clean up comments in urls.py for clarity and maintainability

--- server/urls.py
+++ server/urls.py
@@ -3,16 +3,13 @@ from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
 from .swagger import swagger_urlpatterns
 
-# Language switch URL
 urlpatterns = [
     path('i18n/', include('django.conf.urls.i18n')),
 ]
 
-# Wrap admin and app URLs in i18n_patterns for language prefix support
 urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('api/auth/', include('authapp.urls')),
 )
 
-# Add swagger urls normally
 urlpatterns += swagger_urlpatterns
